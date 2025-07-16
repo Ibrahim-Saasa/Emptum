1d0bb26ffe4a62c251a69228f12247bedb3f85f4
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2025-07-16 20:44:12 +0530

    rebuild project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4 hover:scale-105 transition-transform duration-300">
+        <h2 className="text-2xl font-bold text-gray-900">🔥 Stylish Card</h2>
+        <p className="text-gray-500">
+          Try mixing utility classes and transitions!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
