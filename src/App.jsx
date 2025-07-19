048db10ee7029d4325251d8c2c33e2671f826cec
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2025-07-19 22:54:00 +0530

    congif files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import Header from "./components/header";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4 hover:scale-105 transition-transform duration-300">
-        <h2 className="text-2xl font-bold text-gray-900">🔥 Stylish Card</h2>
-        <p className="text-gray-500">
-          Try mixing utility classes and transitions!
-        </p>
-      </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={"/"} exact={true} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
