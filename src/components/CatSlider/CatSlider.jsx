aabf16f0c8803ed82071186478e8ac16c7286e99
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2025-08-03 19:06:05 +0530

    cat slider

--- src/components/CatSlider/CatSlider.jsx
+++ src/components/CatSlider/CatSlider.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CatSlider = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>This is where the category will go</p>
+    </div>
+  );
 };
 
 export default CatSlider;
